307f29ecc243532bae7cae1bdcbaaf1a0380904d
xxadhana <xxadhana@WPF2KDFT0>
2024-07-15 11:38:06 +0530

    Add project files.

---
+++ TestAppTieto/Calculation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAppTieto
+{
+    public class Calculation
+    {
+        public int add(int x, int y)
+        {
+            return x + y;
+        }
+        public int sub(int x,int y) { return x - y; }   
+    }
+}
